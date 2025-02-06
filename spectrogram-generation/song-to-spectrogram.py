738bf0ae25b51eb0b62e6c6775553a23e2721720
Oscar Chung <oscar.chung.pa@gmail.com>
2025-02-05 20:37:52 -0500

    feat: added files and start of song to spectrogram script

---
+++ spectrogram-generation/song-to-spectrogram.py
@@ -0,0 +1,57 @@
+# TO DO:
+# 1) Go through salami yt pairings' yt IDs
+# 2) Keep list of valid YT links
+# 3) Append the corresponding metadata to each valid salami yt pair
+# 4) Download mp3s for all valid YT videos
+# 5) Create spectrograms from remaining ones
+
+# For now:
+# Get one spectrogram going
+
+# import librosa
+# import librosa.display
+# import matplotlib.pyplot as plt
+# import numpy as np
+import pandas as pd
+import requests
+
+# Store salami id, yt id, and song title for valid songs
+valid_songs = {}
+
+# ----------------------------------------------------------------
+# TABLE ADJUSTMENTS
+data = pd.read_csv("./spectrogram-generation/SALAMI-data/salami_youtube_pairings.csv")
+data2 = pd.read_csv("./spectrogram-generation/SALAMI-data/metadata.csv")
+
+# Merge song name to existing pairings
+final_data = pd.merge(data, data2, how='left',left_on='salami_id', right_on='SONG_ID')
+
+# Remove irrelevant columns
+final_data = final_data[['salami_id', 'youtube_id', 'SONG_TITLE']]
+
+# Renaming columns
+final_data = final_data.rename(columns={'salami_id': 'SALAMI_ID', 'youtube_id': 'YT_ID'})
+
+
+# ----------------------------------------------------------------
+# VALIDATING LINKS
+# Grab current row info
+cur_row = final_data.loc[final_data["SALAMI_ID"] == 3, ["SALAMI_ID", "YT_ID", "SONG_TITLE"]]
+SALAMI_ID = cur_row.iloc[0]["SALAMI_ID"]
+YT_ID = cur_row.iloc[0]["YT_ID"]
+SONG_TITLE = cur_row.iloc[0]["SONG_TITLE"]
+
+# Confirm that YT video is valid
+def try_site(url):
+    request = requests.get(url)
+    return request.status_code == 200
+is_valid = try_site("https://youtu.be/" + YT_ID)
+print(f"The song link for {SONG_TITLE} is valid: {is_valid}")
+
+# Add to List of Valid Songs
+if is_valid:
+    valid_songs[int(SALAMI_ID)] = [YT_ID, SONG_TITLE]
+print(valid_songs)
+
+# TO DO NEXT
+# - 
